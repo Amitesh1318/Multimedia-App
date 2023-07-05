69734eb109511b9f7bf31bfa94f542881b66ca67
Amitesh1318 <sangrah007@gmail.com>
2023-07-05 13:17:57 +0530

    final

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/[4K] Zoro King of Hell - One Piece「AMV Edit].mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Living Life In The Night_128-(DJMaza).mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/RESUME 2.0.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/FBrPZ56XEAAkQUT.jpeg"
+    },
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/nft.jpeg"
+    },
+    {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/wallpapertip_anime-cellphone-wallpaper_2290566.jpg"
+    },
+  ]
